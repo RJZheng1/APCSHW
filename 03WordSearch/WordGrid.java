ed316b17ce07992c18d3aff73022f77597be636c
RJZheng1 <stuykid2016@yahoo.com>
2014-11-19 21:11:25 -0500

    Added constructor

--- 03WordSearch/WordGrid.java
+++ 03WordSearch/WordGrid.java
@@ -1,3 +1,10 @@
 public class WordGrid{
-    
+    private char[][] data;
+    /**Initializes a grid of the specified size and fills all of the positions with spaces.
+     *@param row is the starting height of the WordGrid
+     *@param col is the starting width of the WordGrid
+     */
+    public WordGrid(int rows,int cols){
+	data = new char[rows][cols];
+    }
 }
