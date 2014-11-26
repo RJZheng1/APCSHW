c56cc13960a9fd606d1ff96523e66b2e6f98bcfb
RJZheng1 <stuykid2016@yahoo.com>
2014-11-25 22:13:50 -0500

    Added wordsInPuzzle method

--- 03WordSearch/WordGrid.java
+++ 03WordSearch/WordGrid.java
@@ -8,6 +8,7 @@ public class WordGrid{
     public static void main(String[]args) throws FileNotFoundException{
 	WordGrid g = new WordGrid(20,20);
 	g.loadWordsFromFile("Words.txt",false);
+	System.out.println("Find these words:\n" + g.wordsInPuzzle());
 	System.out.println(g);
     }
     /**Initializes a grid of the specified size and fills all of the positions 
@@ -19,10 +20,25 @@ public class WordGrid{
 	data = new char[rows][cols];
 	clear();
     }
+    public void setSeed(long seed){
+	RNG.setSeed(seed);
+    }
+    /**Set all values in the WordGrid to underscores ' '*/
     private void clear(){
 	for(int a = 0;a < data.length;a++){
 	    for(int b = 0;b < data[a].length;b++)
-		data[a][b] = ' ';
+		data[a][b] = '_';
+	}
+    }
+    /**Adds a random letter to each underscore in the WordGrid
+     */
+    public void addRandomLetters(){
+	Random RNG = new Random();
+	for(int a = 0;a < data.length;a++){
+	    for(int b = 0;b < data[a].length;b++){
+		if(data[a][b] == '_')
+		    data[a][b] = (char)('A' + RNG.nextInt(26));
+	    }
 	}
     }
     /**The proper formatting for a WordGrid is created in the toString.
@@ -38,11 +54,25 @@ public class WordGrid{
 	}
 	return output;
     }
+    public String wordsInPuzzle(){
+	String words = "";
+	for(int x = 0;x < wordsAdded.size();x++){
+	    for(int i = 0;i < 15 - wordsAdded.get(x).length();i++)
+		words += " ";
+	    words += wordsAdded.get(x);
+	    if(x%4 == 3)
+		words += "\n";
+	}
+	return words;
+    }
+    /**Sets the seed of the Random object.
+     *@param seed is the number you want the seed of Random object to be.
+     */
     /**Attempts to add a given word to the specified position of the WordGrid.
      *The word is added from left to right, must fit on the WordGrid and must
      *have a corresponding letter to match any letters that it overlaps.
      *
-     *@param word is any word to be added to the WordGrid.
+     *@param word is the word to be added to the WordGrid.
      *@param row is the vertical location that you want the word to start at.
      *@param col is the horizontal location that you want the word to start at.
      *@param dir is the angle you want the word to be added in, in degrees.
@@ -55,37 +85,25 @@ public class WordGrid{
 	if(dir %45 != 0 || row + word.length() * ydir < -1 || row + word.length() * ydir> data.length || col + word.length() * xdir < -1 || col + word.length() * xdir> data[0].length)
 	    return false;
 	for(int x = 0;x < word.length();x++){
-	    if(data[row][col] == ' '){
+	    if(data[row][col] == '_'){
 		data[row][col] = word.charAt(x);
 		row += ydir;
 		col += xdir;
 	    }else{
 		for(;x > 0;x--){
 		    row -= ydir;
-		    col -=xdir;
-		    data[row][col] = ' ';
+		    col -= xdir;
+		    data[row][col] = '_';
 		}
 		return false;
 	    }
 	}
 	return true;
     }
-    /**Adds a random letter to each empty space in the WordGrid
-     */
-    public void addRandomLetters(){
-	Random RNG = new Random();
-	for(int a = 0;a < data.length;a++){
-	    for(int b = 0;b < data[a].length;b++){
-		if(data[a][b] == ' ')
-		    data[a][b] = (char)('A' + RNG.nextInt(26));
-	    }
-	}
-    }
     /**Takes a text file filed with words and puts them into the WordGrid.
      *@param fileName is the name of the file you wish to read.
      *@param fillRandomLetters is used to decide whether or not to add
-     * random letters to empty spaces.
-     *@return 
+     *random letters to empty spaces.
      */
     public void loadWordsFromFile(String fileName, boolean fillRandomLetters) throws FileNotFoundException{
 	ArrayList<String> words = new ArrayList<String>();
