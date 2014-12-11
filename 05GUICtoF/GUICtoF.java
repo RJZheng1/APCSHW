c27a244d55f2db41aa668ee2b805f8493fea2d23
RJZheng1 <stuykid2016@yahoo.com>
2014-12-10 19:52:16 -0500

    Finished GUICtoF

--- 05GUICtoF/GUICtoF.java
+++ 05GUICtoF/GUICtoF.java
@@ -2,29 +2,55 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class GUICtoF extends JFrame{
+public class GUICtoF extends JFrame implements ActionListener{
     private Container pane;
+    private JTextField celsius;
+    private JTextField fahrenheit;
+    private JLabel l1;
+    private JLabel l2;
     private JButton FtoC;
     private JButton CtoF;
-    private JLabel label;
-    private JTextField textfield;
+
     public GUICtoF(){
 	this.setTitle("Celsius to Fahrenheit Converter");
 	this.setSize(600,400);
 	this.setLocation(100,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
 	pane = this.getContentPane();
-	pane.setLayout(new FlowLayout());
+	pane.setLayout(new GridLayout(3,3));
 	FtoC = new JButton("Convert Fahrenheit to Celsius");
 	CtoF = new JButton("Convert Celsius to Fahrenheit");
-	textfield = new JTextField(12);
-	label = new JLabel("",null,JLabel.CENTER);
-	pane.add(textfield);
+	l1 = new JLabel("Celsius",null,JLabel.LEFT);
+	l2 = new JLabel("Fahrenheit",null,JLabel.LEFT);
+	celsius = new JTextField(40);
+	fahrenheit = new JTextField(40);
+
+	FtoC.setActionCommand("FtoC");
+	FtoC.addActionListener(this);
+	CtoF.setActionCommand("CtoF");
+	CtoF.addActionListener(this);
+
+	pane.add(l1);
+	pane.add(celsius);
+	pane.add(l2);
+	pane.add(fahrenheit);
 	pane.add(FtoC);
 	pane.add(CtoF);
-	pane.add(label);
-
     }
+    public void actionPerformed(ActionEvent e){
+	String action = e.getActionCommand();
+	if(action.equals("FtoC")){
+	    double ftemp = Double.parseDouble(fahrenheit.getText());
+	    ftemp = (ftemp-32)/1.8;
+	    celsius.setText(""+ftemp);
+	}
+	if(action.equals("CtoF")){
+	    double ctemp = Double.parseDouble(celsius.getText());
+	    ctemp = ctemp*1.8+32;
+	    fahrenheit.setText(""+ctemp);
+	}
+    }   
     public static void main(String[] args){
 	GUICtoF converter = new GUICtoF();
 	converter.setVisible(true);
